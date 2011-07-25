a27871d80ab15342530b2a16588a8333fd3cec35
Takashi Masuyama <mamewotoko@gmail.com>
2011-07-25 13:33:48 +0900

    draw bitmap, add alpha channel

--- Hello/src/com/mamewo/hello/TestCircle.java
+++ Hello/src/com/mamewo/hello/TestCircle.java
@@ -2,10 +2,13 @@ package com.mamewo.hello;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.*;
+import android.graphics.drawable.*;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import java.util.*;
 
 public class TestCircle extends Activity {
 	@Override
@@ -19,11 +22,19 @@ public class TestCircle extends Activity {
 class MyCircleView extends View {
 	private float _x = 0;
 	private float _y = 0;
+	private Timer _timer;
+	private String _timestr = "";
+	private long _time = 0;
+	private Bitmap _bitmap = null;
 	
 	public MyCircleView(Context context) {
 		super(context);
 		setFocusable(true);
 		this.setOnTouchListener(new MyMotionListener());
+		_timer = new Timer("hello", true);
+		_timer.schedule(new MyTimerTask(this), 1000, 1000);
+		Resources res = getResources();
+		_bitmap = ((BitmapDrawable)res.getDrawable(R.drawable.img)).getBitmap();		
 	}
 	
 	protected void onDraw(Canvas canvas) {
@@ -36,8 +47,25 @@ class MyCircleView extends View {
 		canvas.drawCircle(150, 150, 100, paint);
 		paint.setColor(Color.RED);
 		paint.setTextSize(24.0f);
-		canvas.drawText(_x + ", " + _y, 10, 40, paint);
+		String coord = String.format("(%3.0f, %3.0f)", _x, _y);
+		canvas.drawText(coord, 10, 40, paint);
+		canvas.drawText(_timestr, 10, 400, paint);
 		canvas.drawRect(new Rect(149, 149, 151, 151), paint);
+		paint.setAlpha(0xAA);
+		canvas.drawBitmap(_bitmap, _x, _y, paint);
+	}
+
+	private class MyTimerTask extends TimerTask {
+		View _v = null;
+		public MyTimerTask (View v) {
+			super();
+			_v = v; 
+		}
+		public void run() {
+			_timestr = "time: " + (_time++);
+			//View.invalidate cannot be called from non-UI thread
+			_v.postInvalidate();
+		}
 	}
 	
 	private class MyMotionListener implements View.OnTouchListener {
