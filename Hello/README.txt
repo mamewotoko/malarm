e147b8d292a5735c3db518b50b92517230efc39d
Takashi Masuyama <mamewotoko@gmail.com>
2011-08-14 22:52:47 +0900

    display alarm date show twitter in Webview add vibration menu and permission add playlist as separate class

--- Hello/README.txt
+++ Hello/README.txt
@@ -2,6 +2,12 @@
 
 This is a sample application of android (version 2.3.3).
 
+HOW TO RUN:
+1. Rename Playlist_tmpl.java into Playlist.java.
+2. Fill Playlist.WAKEUP_PLAYLIST and Playlist.SLEEP_PLAYLIST by music
+  filename in /sdcard/music/ folder
+3. Build and run on your target device
+
 MEMO:
 Alarm application starts when user push appwidget.
 After set/stop alarm button
@@ -11,6 +17,7 @@ After set/stop alarm button
 * TODO:
 - editable playlist support
 -- add favorite music
+- vibrate support?
 - add tweet button of now playing
 - link to music store?
 - add license file
--- Hello/src/com/mamewo/hello/HelloActivity.java
+++ Hello/src/com/mamewo/hello/HelloActivity.java
@@ -17,18 +17,26 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.*;
+import android.webkit.*;
+import android.net.http.*;
 
 public class HelloActivity extends Activity implements OnClickListener {
 	/** Called when the activity is first created. */
 	public static final String WAKEUP_ACTION = "com.mamewo.hello.WAKEUP_ACTION";
+	public static final String WAKEUPAPP_ACTION = "com.mamewo.hello.WAKEUPAPP_ACTION";
 	public static final String SLEEP_ACTION = "com.mamewo.hello.SLEEP_ACTION";
 	// 1 hour
 	public static long SLEEP_TIME = 60 * 60 * 1000;
@@ -58,16 +66,45 @@ public class HelloActivity extends Activity implements OnClickListener {
 		_time_label = (TextView) findViewById(R.id.target_time_label);
 		// set default time
 		_webview = (WebView)findViewById(R.id.webView1);
+		WebSettings config = _webview.getSettings();
+		config.setJavaScriptEnabled(true);
+		//to display twitter...
+		config.setDomStorageEnabled(true);
+		config.setSupportZoom(true);
+		_webview.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				_webview.requestFocus();
+				return false;
+			}
+		});
+		final Activity activity = this;
+		_webview.setWebViewClient(new WebViewClient() {
+		   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+		     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+		   }
+		   //for debug
+		   public void onLoadResource (WebView view, String url) {
+			   Log.i("Hello", "loading: " + url);
+		   }
+		   public void onReceivedSslError (WebView view, SslErrorHandler handler, SslError error) {
+			 Toast.makeText(activity, "SSL error " + error, Toast.LENGTH_SHORT).show();
+		   }
+		 });
 	}
 
+	private void loadWebPage () {
+		_webview.loadData("<html><head><title>Loading</title></head><body>Loading....</body></html>", "text/html", "utf-8");
+		_webview.loadUrl("http://twitter.com/");
+	}
+	
 	protected void onStart () {
 		super.onStart();
+		Log.i("Hello", "onStart is called");
 		_time_picker.setCurrentHour(DEFAULT_HOUR);
 		_time_picker.setCurrentMinute(DEFAULT_MIN);
-		//TODO: embed twitter screen?
-		_webview.loadData("<html><head><title>link pane</title></head><body style=\"foreground: white;\"><br><br><h1><a href=\"http://twitter.com/\">Twitter</a></h1></body></html>", "text/html", "UTF-8");
+		loadWebPage();
 	}
-
+	
 	private PendingIntent makePintent(String action) {
 		Intent i = new Intent(this, Player.class);
 		i.setAction(action);
@@ -100,6 +137,11 @@ public class HelloActivity extends Activity implements OnClickListener {
     	case R.id.cancel_alarm:
     		cancelAlarm();
     		break;
+    	case R.id.set_now:
+    		Calendar now = new GregorianCalendar();
+    		_time_picker.setCurrentHour(now.get(Calendar.HOUR_OF_DAY));
+    		_time_picker.setCurrentMinute(now.get(Calendar.MINUTE));
+    		break;
     	default:
     		Log.i("Hello", "Unknown menu");
     		return false;
@@ -107,7 +149,6 @@ public class HelloActivity extends Activity implements OnClickListener {
     	return true;
     }
 	
-	// TODO: implement alarm cancel
 	public void scheduleToPlaylist() {
 		Log.i("Hello", "scheduleToPlaylist is called");
 		//TODO: hide keyboard?
@@ -135,7 +176,8 @@ public class HelloActivity extends Activity implements OnClickListener {
 		}
 		//TODO: make function
 		_time_picker.setEnabled(false);
-		_time_label.setText(String.format("%02d:%02d", target_hour, target_min));
+		//TODO: localize date format?
+		_time_label.setText(String.format("%2d/%2d %02d:%02d", target.get(Calendar.MONTH)+1, target.get(Calendar.DATE), target_hour, target_min));
 		AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 		PendingIntent pendingIntent = makePintent(WAKEUP_ACTION);
 		mgr.set(AlarmManager.RTC_WAKEUP, target_millis, pendingIntent);
@@ -145,25 +187,6 @@ public class HelloActivity extends Activity implements OnClickListener {
 		if (target_millis - now_millis >= SLEEP_TIME) {
 			pendingIntent = makePintent(SLEEP_ACTION);
 			mgr.set(AlarmManager.RTC_WAKEUP, now_millis+SLEEP_TIME, pendingIntent);
-		}
-	}
-
-	void displayBluetoothDevices() {
-		Set<BluetoothDevice> devices = _adapter.getBondedDevices();
-		String message = "";
-		for (BluetoothDevice dev : devices) {
-			message += dev.getName() + ": " + dev.getAddress() + "\n";
-		}
-		showMessage(this.getBaseContext(), message);
-	}
-
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		Log.d("DEBUG", "onActivityResult: " + requestCode + ": " + resultCode);
-		if (requestCode == REQUEST_ENABLE_BT) {
-			if (resultCode != RESULT_OK) {
-				return;
-			}
-			displayBluetoothDevices();
 		}
 	}
 
@@ -186,113 +209,9 @@ public class HelloActivity extends Activity implements OnClickListener {
 		private static MediaPlayer _player = null;
 		private static int _index = 0;
 
-		//put music into music folder of SD card
-		protected static final String[] WAKEUP_PLAYLIST = {
-			"04 星間飛行(マクロスF) 1.m4a", "03 アスパラガス.m4a", "08 DAVID.m4a",
-			"01 Seasons Of Love.m4a", "08 ブルーモーメント.m4a", "01 M☆gic.m4a",
-			"03 銀河 [Album ver.].m4a", "1-17 新しいラプソディー.m4a",
-			"1-12 The Longest Time.m4a", "10 いつでも夢を.m4a", "01 美しく燃える森.m4p",
-			"06 Clap Your Hands Together.m4a", "05 たったった.m4a",
-			"10 好きだって今日は言ったっけ_.m4a", "1-04 ロボッチ [Live].m4a",
-			"1-03 Honesty.m4a", "01 ぶっ生き返す!!.m4a",
-			"04 DEAR LIZ [Live].m4a", "02 maximum the hormone.m4a",
-			"02 ROCK AND ROLL HERO.m4a", "10 虹.m4a", "15 御免ライダー.m4a",
-			"02 心のファンファーレ.m4a", "16 Tong Poo.m4a", "03 my girl.m4a",
-			"05 焔 -ほむら-.m4a", "01 ジ・エンターテイナー.m4a", "17 ヘヘヘイ.m4a",
-			"17 与える男.m4a", "05 地平線を越えて.m4a",
-			"2-05 Butterfly (Delaction Remix).m4a", "17 ハズムリズム.m4a",
-			"1-02 大迷惑.m4a", "2-12 ヨイトマケの唄 [Live].m4a", "14 手引きのようなもの.m4a",
-			"05 I Have Confidence.m4a", "07 ひとりカンタビレのテーマ.m4a",
-			"04 Take Me In Your Arms.m4a", "02 道化師のソネット.m4p",
-			"13 花 -Memento Mori-.m4a", "07 うそつき.m4a", "01 Voice.m4a",
-			"05 Desert On The Moon.m4a", "07 あなたに会いにいこう.m4a",
-			"04 ロボッチ.m4a", "13 夢の帆船.m4a", "13 恋のメガラバ.m4a",
-			"02 BEAT IT.m4a", "09 はじめの一歩.m4a", "11 風の坂道.m4a",
-			"05 威風堂々.m4a", "08 Eight Five Five.m4a", "01 月恋歌.m4a",
-			"2-08 103.m4a", "01 鬱くしき人々のうた.m4a", "6-06 ターンAターン.m4a" };
-
-	private static final String[] SLEEP_PLAYLIST = {
-			"11 約束.m4a",
-			"01 帰れない二人.m4a",
-			"01 Adagio In G Minor.m4a",
-			"07 白鳥.m4a",
-			"09 BEATLES' MEDLEY (This boy ～ I'll be back).m4a",
-			"09 Ave Maria (all . Kall).m4a",
-			"1-07 グリーンスリーヴズによる幻想曲.m4a",
-			"03 THERE IS A SHIP.m4a",
-			"07 モン・アンファン.m4a",
-			"03 吟遊詩人.m4a",
-			"09 As Time Goes By.m4a",
-			"14 THE GARDEN.m4a",
-			"2-12 シシリエンヌ.m4a",
-			"22 Some Day My Prince Will Come.m4a",
-			"08 The Sheltering Sky.m4a",
-			"13 タペストリー.m4a",
-			"09 幻に誘われて.m4a",
-			"08 NORWEGIAN WOOD.m4a",
-			"07 浜辺の歌.m4a",
-			"10 うたの素.m4a",
-			"2-07 G線上のアリア.m4a",
-			"1-06 ロンドンデリーの歌.m4a",
-			"06 彩りの秋.m4a",
-			"19 Ave Maria, Op. 52 No. 6.m4a",
-			"1-12 ヴォカリーズ.m4a",
-			"04 ゆりかご.m4a",
-			"03 Traumerei.m4a",
-			"2-05 愛の悲しみ.m4a",
-			"04 RIVER TO OCEAN.m4a",
-			"16 WHAT A WONDERFUL WORLD.m4a",
-			"05 朝のうち曇り.m4a",
-			"06 最後の子守歌.m4a",
-			"03 G線上のアリア.m4a",
-			"05 WOODSTOCK.m4a",
-			"2-01 無伴奏チェロ組曲第1番～プレリュード_J.S.バッハ.m4a",
-			"04 Misty.m4a",
-			"05 Cry Me A River.m4a",
-			"06 Merry Christmas Mr. Lawrence.m4a",
-			"10 Unforgettable.m4a",
-			"10 Candle On The Water.m4a",
-			"05 Gymnopedies.m4a",
-			"12 Deep River (all. Karr).m4a",
-			"15 ジャパニーズ・ミュージック・ボックス (五木の子守唄).m4a",
-			"07 THE DEEP.m4a",
-			"07 Lullaby.m4a",
-			"02 AMAZING GRACE.m4a",
-			"10 YOU'D BE SO NICE TO COME HOME TO.m4a",
-			"04 花音 ～カノン～.m4a",
-			"11 put your hands up.m4a",
-			"13 Bon Voyage.m4a",
-			"03 アヴェ・マリア.m4a",
-			"1-01 エトピリカ _ Etupirka.m4p",
-			"02 SOMEWHERE SOMETIME.m4a",
-			"03 ZERO LANDMINE-Piano+Cello version-.m4a",
-			"15 Remembrance.m4a",
-			"10 ウォーキング・イン・ジ・エアー.m4a",
-			"10 潮騒 (2002 Live At Gloria Chapel).m4a",
-			"02 Caribbean Blue.m4a",
-			"09 Variations On The Kanon By Pachelbel.m4a",
-			"10 だったん人の踊り.m4a",
-			"01 Living In The Country.m4a",
-			"02 Song.m4a",
-			"01 Misty.m4a",
-			"12 スノーマンズ・ミュージック・ボックス・ダンス.m4a",
-			"05 トロイメライ.m4a",
-			"07 ショパン_ 練習曲 ホ長調, 「別れの曲」.m4a",
-			"03 夜桜 ～yozakura～.m4a",
-			"41 三共『リゲインEB錠』「energy Flow」.m4a",
-			"14 さよならの向う側.m4a",
-			"01 SUMMER TIME ～HOUSE OF THE RISING SUN.m4a",
-			"11 ワタスゲの原.m4a",
-			"04 Air (arr Karr) From Orchestral Suite No. 3 In D Major. BWV..m4a",
-			"10 The Holly And The Ivy.m4a",
-			"06 A Charlie Brown Thanksgiving.m4a", "08 帰り道.m4a",
-			"14 荒城の月 (all. Karr).m4a", "21 Reza (祈り).m4a",
-			"07 Goyang-Goyang.m4a", "15 Angel.m4a", "06 Spring Creek.m4a",
-			"08 Black Stallion.m4a", "01 Colors_Dance.m4a",
-			"06 ゴリラがバナナをくれる日.m4a", "10 Early Morning Range.m4a",
-			"09 Beethoven's Piano sonata No. 8, Pathetique.m4a",
-			"02 Jesus, Jesus, Rest Your Head.m4a",
-			"11 Living Without You.m4a", "07 竹田の子守唄.m4a" };
+		//Please give list of filename as String array
+		private static final String[] SLEEP_PLAYLIST = Playlist.SLEEP_PLAYLIST;
+		private static final String[] WAKEUP_PLAYLIST = Playlist.WAKEUP_PLAYLIST;
 
 		private static String[] current_playlist = SLEEP_PLAYLIST;
 
@@ -314,7 +233,18 @@ public class HelloActivity extends Activity implements OnClickListener {
 				mgr.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
 				mgr.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
 				mgr.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+				mgr.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+				mgr.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+				_index = 0;
 				startMusic(WAKEUP_PLAYLIST);
+				//TODO: vibrate?
+//				Vibrator vib = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
+				//				if (vib != null) {
+				//long pattern[] = { 10, 2000, 500, 1500, 1000, 2000 };
+				//vib.vibrate(pattern, 1);
+				//}
+				//Start app
+				
 			} else if (intent.getAction().equals(SLEEP_ACTION)) {
 				stopMusic();
 				showMessage(context, context.getString(R.string.goodnight));
@@ -357,6 +287,7 @@ public class HelloActivity extends Activity implements OnClickListener {
 			// TODO: use Alarm instead of Thread
 			SleepThread t = new SleepThread(playtime_millis);
 			t.start();
+			_index = 0;
 			startMusic(SLEEP_PLAYLIST);
 		}
 
@@ -370,7 +301,7 @@ public class HelloActivity extends Activity implements OnClickListener {
 		}
 
 		public static class MusicCompletionListener implements
-				MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
+			MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
 			public void onCompletion(MediaPlayer mp) {
 				// TODO: manage index....(sleep / wakeup)
 				Log.i("Hello", "onCompletion listener is called");
@@ -411,7 +342,7 @@ public class HelloActivity extends Activity implements OnClickListener {
 					_index = 0;
 				}
 			}
-			// TODO ... check....
+			// TODO: handle error
 			try {
 				_player.reset();
 				_player.setDataSource(path);
@@ -422,7 +353,27 @@ public class HelloActivity extends Activity implements OnClickListener {
 			}
 		}
 	}
-	//obsolete code...
+
+	//obsoleted code...
+	void displayBluetoothDevices() {
+		Set<BluetoothDevice> devices = _adapter.getBondedDevices();
+		String message = "";
+		for (BluetoothDevice dev : devices) {
+			message += dev.getName() + ": " + dev.getAddress() + "\n";
+		}
+		showMessage(this.getBaseContext(), message);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d("DEBUG", "onActivityResult: " + requestCode + ": " + resultCode);
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode != RESULT_OK) {
+				return;
+			}
+			displayBluetoothDevices();
+		}
+	}
+	
 	public void discoverBluetoothDevices(View v) {
 		Context context = v.getContext();
 		_adapter = BluetoothAdapter.getDefaultAdapter();
