6be3cf6f3fb72bbc113da08473783d122726edc8
Takashi Masuyama <mamewotoko@gmail.com>
2011-12-29 00:28:29 +0900

    change convert tool into perl and normalize filename in playlist by NKFC

---
+++ tool/convert_m3u.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use Unicode::Normalize;
+
+use open IO => qw/:utf8 :std/;
+
+my $content = join('', <STDIN>);
+# more good code?
+$content =~ s/\x0d\x0a|\x0a|\x0d/\n/g;
+my @LINES = split (/\n/, $content);
+foreach my $line (@LINES) {
+    if ($line =~ m/^#/) {
+	print $line."\n";
+	next;
+    }
+    $line =~ s|^.*/||;
+    print NFKC($line)."\n";
+}
--- tool/convert_m3u.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# convert exported play list by iTunes into malarm playlist
-# playlist is exported as _wakeup.m3u and _sleep.m3u
-
-for file in sleep.m3u wakeup.m3u; do
-	perl -pe 's|\r|\n|g' $file | sed -e 's|^[^#].*/||g' > _$file
-done
