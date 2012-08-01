3e53ce2557fd766b66c4d736510db9c1132291f2
Takashi Masuyama <mamewotoko@gmail.com>
2012-08-01 18:38:23 +0900

    fix compiler warnings

--- src/com/mamewo/malarm24/MalarmActivity.java
+++ src/com/mamewo/malarm24/MalarmActivity.java
@@ -19,8 +19,6 @@ import java.util.regex.Pattern;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
@@ -362,7 +360,7 @@ public class MalarmActivity
 	}
 
 	@Override
-	protected void onStart () {
+	protected void onStart() {
 		super.onStart();
 	}
 	
