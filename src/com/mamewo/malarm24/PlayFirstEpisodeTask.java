c20f36c72ec4d68f7942f1ad9de3b16daba7f890
Takashi Masuyama <mamewotoko@gmail.com>
2013-07-04 18:37:50 +0900

    add file

---
+++ src/com/mamewo/malarm24/PlayFirstEpisodeTask.java
@@ -0,0 +1,58 @@
+package com.mamewo.malarm24;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import com.mamewo.lib.podcast_parser.BaseGetPodcastTask;
+import com.mamewo.lib.podcast_parser.EpisodeInfo;
+
+public class PlayFirstEpisodeTask
+	extends BaseGetPodcastTask
+{
+	static final
+	private int TIMEOUT_SEC = 30;
+	EpisodeInfo info_;
+	MediaPlayer player_;
+	static final
+	private String TAG = "malarm";
+	
+	public PlayFirstEpisodeTask(Context context, MediaPlayer player) {
+		super(context, 1, TIMEOUT_SEC, false);
+		player_ = player;
+		info_ = null;
+	}
+	
+	@Override
+	protected void onProgressUpdate(EpisodeInfo... values){
+		if(null != values) {
+			info_ = values[0];
+			Log.d(TAG, "onProgress: url " + info_.url_);
+		}
+	}
+	
+	@Override
+	protected void onPostExecute(Void result) {
+		if(null == info_){
+			Log.d(TAG, "no episode found...");
+			return;
+		}
+		try {
+			Log.d(TAG, "Start playing podcast:" + info_.url_);
+			player_.reset();
+			player_.setDataSource(info_.url_);
+			Log.d(TAG, "prepareAsync");
+			player_.prepareAsync();
+		} catch (IOException e) {
+			//
+			Log.d(TAG, "error set data source: ", e);
+		}
+	}
+	
+//	@Override
+//	protected void onCancelled() {
+//		onFinished();
+//	}
+}
