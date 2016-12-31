20eb8d0aa2a108c3f1c17ddd0eba854ecbf2f2f9
Takashi Masuyama <mamewotoko@gmail.com>
2016-12-31 14:01:33 +0900

    separate sdk install script

---
+++ ci/init_sdk.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# ANDROID_HOME and PATH should be set
+
+echo y | android update sdk --no-ui --filter android-10,android-16,android-24,android-25 # > /dev/null
+echo y | android update sdk --no-ui --filter tools,platform-tools > /dev/null
+echo y | android update sdk --no-ui --filter build-tools-25.1.0 # > /dev/null
+echo y | android update sdk --no-ui --filter build-tools-24.0.3 # > /dev/null
+echo y | android update sdk --no-ui --filter extra-android-m2repository > /dev/null
+
+echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-10
+echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-16
+echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-24
--- ci/snap_ci/init_android.sh
+++ ci/snap_ci/init_android.sh
@@ -10,16 +10,7 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
     # fetch and initialize $ANDROID_HOME
     download-android
 
-    echo y | android update sdk --no-ui --filter android-10,android-16,android-24,android-25 # > /dev/null
-    echo y | android update sdk --no-ui --filter tools,platform-tools > /dev/null
-    echo y | android update sdk --no-ui --all --filter build-tools-25.0.2 # > /dev/null
-    echo y | android update sdk --no-ui --all --filter build-tools-24.0.3 # > /dev/null
-    echo y | android update sdk --no-ui ---all -filter extra-android-m2repository > /dev/null
-
-    echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-10 > /dev/null
-    echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-16 > /dev/null
-    echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-24 > /dev/null
-
+    sh -x ../init_sdk.sh
     ## give up
     #sudo yum update
     #sudo yum install -y glibc.i686
